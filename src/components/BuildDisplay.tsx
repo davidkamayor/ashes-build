528742e277f1463ffbeecb6908329446ce8c9863
David Kamayor <davidkamayor@gmail.com>
2023-07-04 17:44:22 -0400

    add hover window preview to BuildDisplay

--- src/components/BuildDisplay.tsx
+++ src/components/BuildDisplay.tsx
@@ -1,42 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "@mui/material/Link";
 import Container from "@mui/material/Table";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/Table";
 import TableRow from "@mui/material/Table";
 import TableCell from "@mui/material/TableCell";
+import WindowPreview from "./WindowPreview";
 
 function BuildDisplay() {
+  const [showPreview, setShowPreview] = useState(false);
+
+  function handleMouseEnter() {
+    setShowPreview(true);
+  }
+
+  function handleMouseLeave() {
+    setShowPreview(false);
+  }
+
   return (
-    <Container>
-      <h1>Builds by Category</h1>
-      <Table>
-        <TableHead>
+    <div>
+      <Container>
+        <h1>Builds by Category</h1>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Category</TableCell>
+              <TableCell>Working</TableCell>
+              <TableCell>Untested</TableCell>
+            </TableRow>
+          </TableHead>
           <TableRow>
-            <TableCell>Category</TableCell>
-            <TableCell>Working</TableCell>
-            <TableCell>Untested</TableCell>
+            <TableCell>Solo PvX</TableCell>
+            <TableCell>
+              <ul>
+                <li>
+                  <a
+                    href=""
+                    onMouseEnter={() => handleMouseEnter()}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    Meta
+                  </a>
+                </li>
+                <li>
+                  <a href="">Great</a>
+                </li>
+                <li>
+                  <a href="">Good</a>
+                </li>
+                <li>
+                  <a href="">All</a>
+                </li>
+              </ul>
+            </TableCell>
+            <TableCell>
+              <ul>
+                <li>
+                  <a href="">Testing</a>
+                </li>
+                <li>
+                  <a href="">Proposed</a>
+                </li>
+              </ul>
+            </TableCell>
           </TableRow>
-        </TableHead>
-        <TableRow>
-          <TableCell>Solo PvX</TableCell>
-          <TableCell>
-            <ul>
-              <li>Meta</li>
-              <li>Great</li>
-              <li>Good</li>
-              <li>All</li>
-            </ul>
-          </TableCell>
-          <TableCell>
-            <ul>
-              <li>Testing</li>
-              <li>Proposed</li>
-            </ul>
-          </TableCell>
-        </TableRow>
-      </Table>
-    </Container>
+        </Table>
+      </Container>
+      {showPreview ? <WindowPreview></WindowPreview> : ""}
+    </div>
   );
 }
 
---
+++ src/components/WindowPreview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+
+function WindowPreview() {
+  return (
+    <Paper elevation={3} className="p-1">
+      INTERACTIVE PREVIEW
+    </Paper>
+  );
+}
+
+export default WindowPreview;
